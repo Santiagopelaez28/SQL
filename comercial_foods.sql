f544532310bea852a8d22cef996b5c22f12479c3
Santiagopelaez28 <159547983+Santiagopelaez28@users.noreply.github.com>
2024-03-21 10:09:19 -0500

    Add files via upload

--- comercial_foods.sql
+++ comercial_foods.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-03-2024 a las 15:06:22
+-- Tiempo de generación: 21-03-2024 a las 16:07:54
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
